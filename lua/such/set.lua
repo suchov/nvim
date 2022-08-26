3fcada2eb11ec2160a505386758b9cbcac037b93
Artem Sychov <[artem.suchov@gmail.com]>
2022-08-26 18:54:51 +0200

    add some more git aliaces

--- lua/such/set.lua
+++ lua/such/set.lua
@@ -2,6 +2,10 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 16
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.guicursor = ""
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
