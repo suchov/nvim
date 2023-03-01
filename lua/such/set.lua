21335ae0089f0561ec0cbedad23f8ce5f45f321a
Artem Sychov <[artem.suchov@gmail.com]>
2023-03-01 09:27:29 +0100

    a tone of changes form the primagen

--- lua/such/set.lua
+++ lua/such/set.lua
@@ -2,19 +2,27 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 16
-vim.opt.colorcolumn = "90"
-vim.opt.signcolumn = "yes"
-vim.opt.guicursor = ""
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim opt swapfile = false
+vim opt backup = false
+vim opt undodir = os getenv("HOME") .. "/.vim/undodir"
+vim opt unofile = true
+
+vim.opt.scrolloff = 16
+vim.opt.colorcolumn = "90"
+vim.opt.signcolumn = "yes"
+vim.opt.guicursor = ""
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.smartindent = true
 
 vim.opt.wrap = false	
 
