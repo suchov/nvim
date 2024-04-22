2ea992c8a5862fea4e2242b5345332026e70e214
Artem Sychov <[artem.suchov@gmail.com]>
2024-04-22 17:05:12 +0200

    some fixes and comments

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
+-- https://github.com/nvim-telescope/telescope.nvim
 local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>sw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
