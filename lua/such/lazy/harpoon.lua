4c6946b90e21019d70bc6668a394321f41c821bb
Artem Sychov <artem@sychov.dev>
2024-04-24 00:01:25 +0200

    finish the lazy stuff - at least now it's working and harpoon too - need to think about it's maping

--- lua/such/lazy/harpoon.lua
+++ lua/such/lazy/harpoon.lua
@@ -15,19 +15,17 @@ return {
 
         --vim.keymap.set("n", "<leader>a", mark.add_file)
         --vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
         --vim.keymap.set("n", "<C-y>", function() ui.nav_file(1) end)
         --vim.keymap.set("n", "<C-i>", function() ui.nav_file(2) end)
         --vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
         --vim.keymap.set("n", "<C-m>", function() ui.nav_file(4) end)
     end,
-    keys = {
-      { "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file", },
-      { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
-      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
-      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
-      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
-      { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
-    },
+        keys = {
+            { "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file", },
+            { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+            { "<C-y>", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 3", },
+            { "<C-i>", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 4", },
+            { "<C-n>", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 1", },
+            { "<C-m>", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 2", },
+        },
 }
