4f68e9a6651bd1737a67455de521a3d9fcd976f8
Artem Sychov <[artem.suchov@gmail.com]>
2024-10-08 22:02:37 +0200

    some updates to the harpoon key maping

--- lua/such/lazy/harpoon.lua
+++ lua/such/lazy/harpoon.lua
@@ -1,10 +1,7 @@
 -- https://github.com/ThePrimeagen/harpoon
 
---local mark = require("harpoon.mark")
---local ui = require("harpoon.ui")
-
 return {
-    "theprimeagen/harpoon",
+    "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
 
@@ -13,19 +10,16 @@ return {
 		---@diagnostic disable-next-line: missing-parameter
 		harpoon:setup()
 
-        --vim.keymap.set("n", "<leader>a", mark.add_file)
-        --vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-        --vim.keymap.set("n", "<C-y>", function() ui.nav_file(1) end)
-        --vim.keymap.set("n", "<C-i>", function() ui.nav_file(2) end)
-        --vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-        --vim.keymap.set("n", "<C-m>", function() ui.nav_file(4) end)
+        -- rewrite a key maping a bit
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-y>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-u>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(1) end)
+
     end,
-        keys = {
-            { "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file", },
-            { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-            { "<C-3>", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 3", },
-            { "<C-4>", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 4", },
-            { "<C-1>", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 1", },
-            { "<C-2>", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 2", },
-        },
+    keys = {
+        { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+    },
 }
