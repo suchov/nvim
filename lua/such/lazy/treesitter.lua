8c94a7022ffdaf913b27631eb38d7431463432c8
Artem Sychov <artem@sychov.dev>
2025-07-31 14:41:32 +0200

    some changes to my dot files

--- lua/such/lazy/treesitter.lua
+++ lua/such/lazy/treesitter.lua
@@ -5,8 +5,8 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
+                --"vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                --"jsdoc", "bash",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
--- lua/such/remap.lua
+++ lua/such/remap.lua
@@ -35,6 +35,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Change the mod of the file to make it executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
